d28bc6169fe0b52f46b1300b512ef8a6fa6b0265
pommicket <pommicket@gmail.com>
2025-01-30 22:43:02 +0000

    Output LaTeX table for Table 6

--- artifact_evaluation/README.md
+++ artifact_evaluation/README.md
@@ -9,7 +9,7 @@ cd amulet/docker
 docker attach invisispec
 ```
 
-Run `Table_4_uarch_trace_formats.sh` - this will output a LaTeX table to `Table_4_Results.tex`,
+Run `Table_4_uarch_trace_formats.sh` - this will output a LaTeX document containing the table to `Table_4_Results.tex`,
 and a plaintext listing to `Table_4_Results.txt`.
 
 You can also provide the number of programs as an argument, e.g. `./Table_4_uarch_trace_formats.sh 10`,
@@ -20,21 +20,8 @@ if you want to try testing with a smaller number of programs first.
 
 This should be run inside of the InvisiSpec docker container.
 
-Run `Table_6_Smaller_uarch_structures.sh` - this will output to `Table_6_Results.txt`.
-The output format will be
-
-```
-Smaller uarch structures - 200 programs
-=== baseline ===
-detected_violation: YES/NO
-campaign_execution_time: <time in seconds>
-=== reduced_cache ===
-detected_violation: YES/NO
-campaign_execution_time: <time in seconds>
-=== reduced_cache_and_mshrs ===
-detected_violation: YES/NO
-campaign_execution_time: <time in seconds>
-```
+Run `Table_6_Smaller_uarch_structures.sh` - this will output a LaTeX document containing the table to
+`Table_6_Results.tex` and a plaintext listing to `Table_6_Results.txt`.
 
 You can also provide the number of programs as an argument, e.g. `./Table_6_Smaller_uarch_structures.sh 10`,
 if you want to try testing with a smaller number of programs first.
--- artifact_evaluation/Table_6_Smaller_uarch_structures.sh
+++ artifact_evaluation/Table_6_Smaller_uarch_structures.sh
@@ -4,20 +4,34 @@
 PROGRAMS="${1:-200}"
 
 RESULTS=/code/revizor-docker/artifact_evaluation/Table_6_Results.txt
+RESULTS_TEX=/code/revizor-docker/artifact_evaluation/Table_6_Results.tex
 echo "Smaller uarch structures - $PROGRAMS programs" > $RESULTS
+
+cat >$RESULTS_TEX <<EOF
+\documentclass{article}
+\begin{document}
+
+Table 6, generated with $PROGRAMS programs
+
+\begin{tabular}{lll}\hline
+{\bf InvisiSpec Configuration}&{\bf Time}&{\bf Violation}\\\\\\hline
+EOF
 
 cd /code/revizor-docker/src
 for row in 1 2 3; do
     case $row in
         1)
             config=../docker/docker_invisispec/docker_gem5_v1_final_cache_and_tlb_patched.yaml
-            name=baseline ;;
+            name=baseline
+            fullname='Patched, 8-way L1D, 256 MSHRs';;
         2)
             config=../docker/docker_invisispec/docker_gem5_v1_final_cache_and_tlb_patched_reduced_cache.yaml
-            name=reduced_cache ;;
+            name=reduced_cache
+            fullname='Patched, 2-way L1D, 256 MSHRs';;
         3)
             config=../docker/docker_invisispec/docker_gem5_v1_final_cache_and_tlb_patched_reduced_cache_and_mshrs.yaml
-            name=reduced_cache_and_mshrs ;;
+            name=reduced_cache_and_mshrs
+            fullname='Patched, 2-way L1D, 2 MSHRs';;
     esac
     echo "Running benchmark $name"
     python3.11 benchmark.py --extra-args='--ruby,--InvisiSpec,--InvisiSpec_Futuristic,-s,x86/isa_spec/base.json'\
@@ -26,6 +40,14 @@ for row in 1 2 3; do
     echo "=== $name ===" >> $RESULTS
     grep 'detected_violation' benchmark-out-$name/info.txt >> $RESULTS
     grep 'campaign_execution_time' benchmark-out-$name/info.txt >> $RESULTS
-
-
+    execution_time=$(grep 'campaign_execution_time' benchmark-out-$name/info.txt | cut -d':' -f2)
+    detected_violation=$(grep 'detected_violation' benchmark-out-$name/info.txt | cut -d':' -f2)
+    execution_time=$(python3 -c "print(round($execution_time / 60))")" min"
+    echo "$fullname & $execution_time & $detected_violation \\\\\\\\" >> $RESULTS_TEX
 done
+
+cat >>$RESULTS_TEX <<EOF
+\hline
+\end{tabular}
+\end{document}
+EOF
