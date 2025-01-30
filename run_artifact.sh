48cfb97c6cf3a7c28f31052aabefa9f01f623fd4
pommicket <pommicket@gmail.com>
2025-01-30 23:45:15 +0000

    Stop invisispec container in run_artifact.sh

--- run_artifact.sh
+++ run_artifact.sh
@@ -3,6 +3,7 @@
 cd docker
 
 echo 'Generating Table 4...'
+./dockerRun.sh invisispec stop
 ./dockerRun.sh invisispec start uarch_trace_formats
 while [ '!' '-f' 'docker_invisispec/revizor-docker/artifact_evaluation/Table_4_Results.tex' ]; do
 	sleep 30
@@ -10,6 +11,7 @@ done
 
 
 echo 'Generating Table 6...'
+./dockerRun.sh invisispec stop
 ./dockerRun.sh invisispec start smaller_uarch_structures
 while [ '!' '-f' 'docker_invisispec/revizor-docker/artifact_evaluation/Table_6_Results.tex' ]; do
 	sleep 30
