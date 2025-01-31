fb6c287cc9b3f04eb747b60c4ea6202b51052068
pommicket <pommicket@gmail.com>
2025-01-31 01:11:21 +0000

    Fix wait condition for Table 6

--- run_artifact.sh
+++ run_artifact.sh
@@ -12,12 +12,19 @@ echo 'Generating Table 4...'
 while [ '!' '-f' 'docker_invisispec/revizor-docker/artifact_evaluation/Table_4_Results.tex' ]; do
 	sleep 30
 done
+# Wait for Python script to finish writing
+sleep 10
 cp docker_invisispec/revizor-docker/artifact_evaluation/Table_4_Results* ../artifact_evaluation/
 
 echo 'Generating Table 6...'
 ./dockerRun.sh invisispec stop
 ./dockerRun.sh invisispec start smaller_uarch_structures
-while [ '!' '-f' 'docker_invisispec/revizor-docker/artifact_evaluation/Table_6_Results.tex' ]; do
+while true; do
 	sleep 30
+	result_file=docker_invisispec/revizor-docker/artifact_evaluation/Table_6_Results.tex
+	# Wait for tex file to contain "end{document}"
+	if [ '-f' $result_file ] && grep 'end{document}' $result_file; then
+		break
+	fi
 done
 cp docker_invisispec/revizor-docker/artifact_evaluation/Table_6_Results* ../artifact_evaluation/
